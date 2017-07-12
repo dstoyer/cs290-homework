e159f72ab4681dc4628d4224397c7ab94e907384
Dan Stoyer <stoyerd@oregonstate.edu>
2017-07-12 00:31:08 -0700

    added original broken code to fix_closure.js

--- hw3/fix_closure.js
+++ hw3/fix_closure.js
@@ -4,12 +4,32 @@
  * Date: Jul 7, 2017
  */
 
+/* **** Original broken code ****
+//function buildList(list) {
+//    var result = [];
+//    for (var i = 0; i < list.length; i++) {
+//        var item = 'item' + list[i];
+//        result.push( function() {alert(item + ' ' + list[i])} );
+//    }
+//    return result;
+//}
+// 
+//function testList() {
+//    var fnlist = buildList([1,2,3]);
+//    // using j only to help prevent confusion - could use i
+//    for (var j = 0; j < fnlist.length; j++) {
+//        fnlist[j]();
+//    }
+//}
+**********************************/
+
 function buildList(list) {
     var result = [];
     for (var i = 0; i < list.length; i++) {
         result.push( function(idx) {
     		return function(){
-    			console.log('item[' + list[idx] + '] ' + list[idx]);
+    			var item = 'item[' + list[idx] + '] ';
+    			console.log(item + list[idx]);
 			}; 
         }(i)); // The last part (i) calls the function immediately.
     }
